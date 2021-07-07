ade2d76bea6cae453e9a3d58ce3883c2b38ec878
taniryla <ryland0000@gmail.com>
2021-07-06 21:22:44 -0600

    completed

--- src/index.js
+++ src/index.js
@@ -3,43 +3,75 @@ console.log('project is up')
 // 👉 TASK 1- Select the following elements from the DOM:
 
 // A- BUTTONS
-const launchButton = null
-const confirmButton = null
-const cancelButton = null
+const launchButton = document.querySelector('#launchButton');
+const confirmButton = document.querySelector('#confirmButton');
+const cancelButton = document.querySelector('#cancelButton');
 
 // B- MESSAGES
-const successMessage = null
-const failureMessage = null
+const successMessage = document.querySelector('.report.success');
+const failureMessage = document.querySelector('.report.failure')
 
 // C- MODAL
-const modal = null
+const modal = document.querySelector('div.modal');
 
 
 // 👉 TASK 2- Demo handling click events on button#launchButton, using:
 //  A- HTML inside index.html
 //  B- The DOM's element.onclick attribute
+launchButton.onclick = function (event){
+    console.log(event.target);
+    console.log('overwriting the onclick attribute');
+}
+
 //  C- element.addEventListener('click', callback)
+
+launchButton.addEventListener('click', event => {
+    console.log('this is the event from addEventListner');
+    console.log(event.type, event.timeStamp);
+})
 
 
 // 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
 
+function launch(event) {
+    modal.classList.remove('off');
+}
+
+launchButton.addEventListener('click', launch);
 
 // 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
 // Add it as a listener for clicks on the confirmation button.
 
+function confirm(event) {
+    modal.classList.add('off');
+    successMessage.classList.remove('off');
+}
 
+confirmButton.addEventListener('click', confirm);
 // 👉 TASK 5- Create a function to cancel the launch.
 // It should close the modal and display a failure report.
 // Add it as a listener for clicks on the cancellation button.
 
+function cancel(event) {
+    modal.classList.add('off');
+    failureMessage.classList.remove('off');
+}
+
+cancelButton.addEventListener('click', cancel);
 
 // 👉 TASK 6- Create a function that closes the modal if
 // the user hits the Escape key on their keyboard.
 // Add it as an event listener for 'keydown' events on document.
 
+function escKey(event) {
+   if (event.keyCode === 27) {
+       modal.classList.add('off');
+   }
+}
+document.addEventListener('keydown', escKey);
 
 // 👉 TASK 7- Add to ALL ELEMENTS ON THE PAGE an event listener for click events.
 // It should console.log the target 🎯 of the event.
